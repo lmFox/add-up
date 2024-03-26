7b74fd305883a2f4c4e4970b978015f32cba4b95
levi.vos <levi.vos@technolution.nl>
2024-03-26 08:18:04 +0100

    Added test cases to view component.

--- app/src/app/components/view/view.component.spec.ts
+++ app/src/app/components/view/view.component.spec.ts
@@ -27,12 +27,21 @@ describe('ViewComponent', () => {
             expect(component.showAnswer).toBeFalse();
             component.next();
 
-            expect(component.current).toEqual(exercise);
+            expect(component.current).toEqual(exercise);            
             expect(component.showAnswer).toBeTrue();
             component.next();
         });
 
         expect(component.current).toBeUndefined();
+    });
+
+    it('should show end screen after all exercises', () => {
+        component.exercises.forEach(_exercise => {
+            expect(component.showEndScreen).toBeFalse();
+            component.next();
+            component.next();
+        });
+
         expect(component.showEndScreen).toBeTrue();
     });
 });
