7443fd50dc12c8a9a7a5fa70cea57e5aa8f4db7d
levi <l.m.vos@umail.leidenuniv.nl>
2024-04-05 12:06:28 +0200

    Wrote unit test for exercise model.

---
+++ app/src/app/domain/exercise.spec.ts
@@ -0,0 +1,11 @@
+import { Exercise } from "./exercise";
+import { Operation } from "./operation";
+
+describe('Exercise', () => {
+    it('should calculate correct answers', () => {
+        expect(new Exercise(Operation.Addition, 1, 2).answer()).toBe(3);
+        expect(new Exercise(Operation.Subtraction, 7, 3).answer()).toBe(4);
+        expect(new Exercise(Operation.Multiplication, 2, 3).answer()).toBe(6);
+        expect(new Exercise(Operation.Division, 12, 6).answer()).toBe(2);
+    });
+});
