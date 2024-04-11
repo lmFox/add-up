61c7e10012ac58f1a13bc25d6dabac7b4dba66f5
levi.vos <levi.vos@technolution.nl>
2024-04-11 15:48:42 +0200

    Improved generation of multiplications.

--- app/src/app/domain/generators/multiplication-generator.ts
+++ app/src/app/domain/generators/multiplication-generator.ts
@@ -1,7 +1,21 @@
 import { Operation } from "domain/operation";
-import { BaseGenerator } from "./base-generator";
+import { BaseGenerator, GenerateOptions } from "./base-generator";
+import { Difficulty } from "domain/difficulty";
 
 export class MultiplicationGenerator extends BaseGenerator {
+    protected override readonly options = new Map<Difficulty, GenerateOptions[]>([
+        [Difficulty.One, [{ lhsSize: 1, rhsSize: 1 }]],
+        [Difficulty.Two, [
+            { lhsSize: 1, rhsSize: 2 },
+            { lhsSize: 2, rhsSize: 1 },
+        ]],
+        [Difficulty.Three, [
+            { lhsSize: 2, rhsSize: 2 },
+            { lhsSize: 3, rhsSize: 1 },
+            { lhsSize: 1, rhsSize: 3 }
+        ]]
+    ]);
+    
     protected override get operation(): Operation {
         return Operation.Multiplication;
     }
