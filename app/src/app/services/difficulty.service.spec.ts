d1131b09aab7b22192726122b38df04e6cae6ef8
levi <l.m.vos@umail.leidenuniv.nl>
2024-07-12 09:44:41 +0200

    Added unit tests for difficulty service.

--- app/src/app/services/difficulty.service.spec.ts
+++ app/src/app/services/difficulty.service.spec.ts
@@ -1,16 +1,46 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DifficultyService } from './difficulty.service';
+import { Difficulty } from 'domain/difficulty';
+import { Operation } from 'domain/operation';
 
 describe('DifficultyService', () => {
-  let service: DifficultyService;
+    let service: DifficultyService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DifficultyService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(DifficultyService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should lookup difficulties from local storage', () => {
+        const expectedDifficulty = Difficulty.Max;
+        const localStorageSpy = spyOn(window.localStorage, 'getItem')
+                                    .and.returnValue(expectedDifficulty.toString());
+
+        expect(service.lookup(Operation.Addition)).toBe(expectedDifficulty);
+        expect(localStorageSpy).toHaveBeenCalled();
+    });
+
+    it('should return lowest difficulty when not found in local storage', () => {
+        spyOn(window.localStorage, 'getItem').and.returnValue(null);
+
+        expect(service.lookup(Operation.Addition)).toBe(Difficulty.One);
+    });
+
+    it('should return lowest difficulty when incorrect value found in local storage', () => {
+        const dirty = ['-1', (Difficulty.Max + 1).toString(), 'asdf'];
+
+        spyOn(window.localStorage, 'getItem').and.returnValues(...dirty);
+
+        dirty.forEach(() => {
+            expect(service.lookup(Operation.Addition)).toBe(Difficulty.One);
+        });
+    });
+
+    it('should write difficulty to local storage', () => {
+        const localStorageSpy = spyOn(window.localStorage, 'setItem').and.stub();
+        service.set(Operation.Addition, Difficulty.Max);
+
+        expect(localStorageSpy).toHaveBeenCalledOnceWith(Operation.Addition, Difficulty.Max.toString());
+    });
 });
