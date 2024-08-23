312a46e4d0de7d80abea53ffbd51664311091f24
levi <l.m.vos@umail.leidenuniv.nl>
2024-08-23 12:39:23 +0200

    Added timer to view component.

--- app/src/app/components/view/view.component.ts
+++ app/src/app/components/view/view.component.ts
@@ -4,6 +4,9 @@ import { Exercise } from 'domain/exercise';
 import { SettingsComponent } from 'components/settings/settings.component';
 import { ExerciseComponent, EShowExercise } from 'components/exercise/exercise.component';
 import { IProgress } from 'domain/i-progress';
+import { Subscription, timer } from 'rxjs';
+
+const TIMER_DURATION_MS = 3000;
 
 @Component({
     selector: 'app-view',
@@ -16,6 +19,7 @@ export class ViewComponent {
     private index: number = 0;
     private exercises: Exercise[] = [];
     private _show = EShowExercise.Exercise;
+    private timer?: Subscription;
     showSettings = false;
 
     get current(): Exercise | undefined {
@@ -46,6 +50,7 @@ export class ViewComponent {
         this.exercises = this.exerciseService.generate();
         this._show = EShowExercise.Exercise;
         this.showSettings = false;
+        this.resetTimer();
     }
 
     next() {
@@ -53,11 +58,18 @@ export class ViewComponent {
             this.proceedNextExercise();
         } else {
             this._show = EShowExercise.Answer;
+            this.timer?.unsubscribe();
         }
     }
 
     private proceedNextExercise() {
         this.index += 1;
         this._show = EShowExercise.Exercise;
+        this.resetTimer();
+    }
+
+    private resetTimer() {
+        this.timer?.unsubscribe();
+        this.timer = timer(TIMER_DURATION_MS).subscribe(() => this._show = EShowExercise.None);
     }
 }
